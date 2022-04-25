5932d44bb48f122572f8ac75b8ed84f32369746e
Michael Zechner <michael-zechner@gmx.at>
2022-04-25 23:25:40 +0200

    navbar, footer, home-draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { MyFootComponent } from './my-foot/my-foot.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    MyFootComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-foot/my-foot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-foot',
+  templateUrl: './my-foot.component.html',
+  styleUrls: ['./my-foot.component.scss']
+})
+export class MyFootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
