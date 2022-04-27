bf4eac74e180725076e4ba02b40db515265c7a58
Michael Zechner <michael-zechner@gmx.at>
2022-04-27 10:15:02 +0200

    basic stories component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StoriesComponent } from './stories/stories.component';
 
 const routes: Routes = [
   {
     path:"",component:HomeComponent
+  },
+  {
+    path:"stories",component:StoriesComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { MyFootComponent } from './my-foot/my-foot.component';
 import { HomeComponent } from './home/home.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNavComponent,
     MyFootComponent,
-    HomeComponent
+    HomeComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
