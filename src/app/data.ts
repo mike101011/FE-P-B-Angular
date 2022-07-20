c501d104942dbb473d483842640e0052b37f855a
Michael Zechner <michael-zechner@gmx.at>
2022-07-20 23:16:15 +0200

    Adds news cards and completes hero

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,1 +1,24 @@
 //Store all relevant data here in arrays!
+export const news:Array<any>=[
+    {
+        pic:"https://nationaltoday.com/wp-content/uploads/2020/03/Major-League-Baseball-Opening-Day-300x300.jpg",
+        title:"Alumni Base Ball Game ",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quas optio cumque laboriosam, iusto itaque."
+    },
+    {
+        pic:"https://enkoeducation.com/wp-content/uploads/2020/04/fordham-university-300x300.jpg",
+        title:"New Campus Garden",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quas optio cumque laboriosam, iusto itaque."
+    },
+    {
+        pic:"https://www.csh.ac.at/wp-content/uploads/2017/07/Simon-DeDeo-500x500-low-qual-300x300.jpg",
+        title:"Lecture by Simon Deodeo",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quas optio cumque laboriosam, iusto itaque."
+    },
+    {
+        pic:"https://cdn.fh-joanneum.at/media/2018/10/pro-carton-young-design-award-2018-300x300.jpg",
+        title:"Design Award To Alumni",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quas optio cumque laboriosam, iusto itaque."
+    },
+
+]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {news} from "../data"
 
 @Component({
   selector: 'home',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  news=news
   constructor() { }
 
   ngOnInit(): void {
